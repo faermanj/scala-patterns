2b633f71f9415bc108adcbff8c2ede52a338ea01
Julio Faerman <jfaerman@gmail.com>
2013-09-18 08:54:28 -0500

    Thanks Stack Overflow: http://stackoverflow.com/questions/18864530/how-to-fetch-a-set-of-pages-as-a-stream

--- src/main/scala/streaming/Pages.scala
+++ src/main/scala/streaming/Pages.scala
@@ -6,27 +6,25 @@ case class Page(number: Int)
 case class Pages(pages: Seq[Page], truncated: Boolean)
 
 class PagesService(pageSize: Int, pagesServed: Int) {
-  def toPage(i: Int) = new Page(i)
-
   def getPages =
-    Pages((1 to pageSize).map(toPage), pageSize < pagesServed)
+    Pages((1 to pageSize).map(Page), pageSize < pagesServed)
 
   def nextPages(previous: Pages) = {
     val first = previous.pages.last.number + 1
     val last = min(first + pageSize, pagesServed)
-    Pages((first to last).map(toPage), last < pagesServed)
+    Pages((first to last).map(Page), last < pagesServed)
   }
 }
 
 object PagesClient extends App {
-	val service = new PagesService(10,100)
-	
-	val first = service.getPages
-	assert(first.truncated)
-	first.pages.foreach(println(_))
-	
-	val second = service.nextPages(first)
-	second.pages.foreach(println(_))
-	
-	val book:Stream[Page] = ???
+  val service = new PagesService(10, 100)
+
+  val chapters: Stream[Pages] = service.getPages #::
+    chapters
+    .map(service.nextPages)
+    .takeWhile(_.pages.nonEmpty)
+
+  val book: Stream[Page] = chapters.flatten(_.pages)
+
+  book take 50 foreach { println(_) }
 }
