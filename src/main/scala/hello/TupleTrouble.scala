e90d4f75c180475203f1d34eae3262083258fa18
Julio Faerman <jfaerman@gmail.com>
2016-02-13 17:33:07 -0200

    Concise syntax for [flat]Map + pattern matching

---
+++ src/main/scala/hello/TupleTrouble.scala
@@ -0,0 +1,8 @@
+package hello
+
+object TupleTrouble extends App {
+  def div(a:Int,b:Int):(Int,Int) = (a/b, a%b)
+  
+  val (result,remain) = div (9,4)
+  println(s"result: $result remain: $remain") 
+}
